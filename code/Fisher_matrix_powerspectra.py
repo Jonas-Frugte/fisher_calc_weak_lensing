18356364307fb91847bebe94eb9be0a7ded92a24
Jonas Frugte <jonasfrugte@gmail.com>
2025-05-08 13:29:14 +0200

    takada jain fisher spectra

--- code/Fisher_matrix_powerspectra.py
+++ code/Fisher_matrix_powerspectra.py
@@ -25,13 +25,21 @@ def fisher_calc_wrapper_planck(args, tracers):
     # Fisher_mat_single(int lmin, int lminbin, int lmax, int triangle_step_size, int num_bispec_samples, char* par1, char* par2, int num_cores, char* type)
     if tracers == 'c':
         return vispy.Fisher_powersp_single(8, 400, b'c', par1 = pars[i], par2 = pars[j])
+    
+def fisher_calc_wrapper_takada_jain(args, tracers):
+    i, j = args
+    # Fisher_mat_full(int lmin, int lminbin, int lmax, int triangle_step_size, int num_bispec_samples, char* par1, char* par2, int num_cores)
+    # Fisher_mat_single(int lmin, int lminbin, int lmax, int triangle_step_size, int num_bispec_samples, char* par1, char* par2, int num_cores, char* type)
+    if tracers == 's':
+        # lmax should actually be 3000, but interpolation currently doesn't go that high
+        return vispy.Fisher_powersp_single(50, 2000, b's', par1 = pars[i], par2 = pars[j])
 
-for tracer in ['c']: #['c', 's', 'both']:
+for tracer in ['s']: #['c', 's', 'both']:
     mat = np.zeros((len(pars), len(pars)))
     for i, j in product(range(len(pars)), repeat = 2):
-        result = fisher_calc_wrapper_planck((i, j), tracer)
+        result = fisher_calc_wrapper_takada_jain((i, j), tracer)
         mat[i, j] = result
         mat[j, i] = result  # Symmetric assignment
 
-    np.savetxt(f'fisher_matrices/fish_mat_powersp_{tracer}_planck.txt', mat)
+    np.savetxt(f'fisher_matrices/fish_mat_powersp_{tracer}_takada_jain.txt', mat)
     print(mat)
